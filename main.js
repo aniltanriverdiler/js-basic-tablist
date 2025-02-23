300f758172561a83d593e160144027d1dc961208
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-02-23 10:12:40 +0300

    feat: Add buttons background color in js

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ function getLondonInfo() {
 
   let closeCityInfoTokyo = document.getElementById('tab3-content');
   closeCityInfoTokyo.classList.add('d-none');
+
+
+  let buttonOneActiveBackgroundColor = document.getElementById('tab1-button');
+  buttonOneActiveBackgroundColor.classList.add('bg-secondary', 'text-white');
+
+  let buttonTwoDisabledBackgroundColor = document.getElementById('tab2-button');
+  buttonTwoDisabledBackgroundColor.classList.remove('bg-secondary', 'text-white');
+  
+  let buttonThreeDisabledBackgroundColor = document.getElementById('tab3-button');
+  buttonThreeDisabledBackgroundColor.classList.remove('bg-secondary', 'text-white');
 }
 
 function getParisInfo() {
@@ -18,6 +28,16 @@ function getParisInfo() {
 
   let closeCityInfoTokyo = document.getElementById('tab3-content');
   closeCityInfoTokyo.classList.add('d-none');
+
+  
+  let buttonTwoActiveBackgroundColor = document.getElementById('tab2-button');
+  buttonTwoActiveBackgroundColor.classList.add('bg-secondary', 'text-white');
+
+  let buttonOneDisabledBackgroundColor = document.getElementById('tab1-button');
+  buttonOneDisabledBackgroundColor.classList.remove('bg-secondary', 'text-white');
+  
+  let buttonThreeDisabledBackgroundColor = document.getElementById('tab3-button');
+  buttonThreeDisabledBackgroundColor.classList.remove('bg-secondary', 'text-white');
 }
 
 function getTokyoInfo() {
@@ -29,4 +49,15 @@ function getTokyoInfo() {
 
   let closeCityInfoParis = document.getElementById('tab2-content');
   closeCityInfoParis.classList.add('d-none');
+
+
+  let buttonThreeActiveBackgroundColor = document.getElementById('tab3-button');
+  buttonThreeActiveBackgroundColor.classList.add('bg-secondary', 'text-white');
+
+  let buttonTwoDisabledBackgroundColor = document.getElementById('tab2-button');
+  buttonTwoDisabledBackgroundColor.classList.remove('bg-secondary', 'text-white');
+  
+  let buttonOneDisabledBackgroundColor = document.getElementById('tab1-button');
+  buttonOneDisabledBackgroundColor.classList.remove('bg-secondary', 'text-white');
 }
+
